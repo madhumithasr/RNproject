ff08867ec3daf8d7d3932dc2c294bbf8041ace07
madhumithasr <madhukaan@gmail.com>
2023-02-25 11:26:47 +0530

    files commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,14 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        'endOfLine': 'auto',
+        'singleQuote': true,
+        'parser': 'flow',
+      },
+    ],
+  },
 };
